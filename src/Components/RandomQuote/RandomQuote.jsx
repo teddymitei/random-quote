17a4bae5a4ad1ee0e3afef08a02cca5b56d6aec5
Maverick-ke <106772779+Maverick-ke@users.noreply.github.com>
2024-06-24 13:57:16 +0300

    update

--- src/Components/RandomQuote/RandomQuote.jsx
+++ src/Components/RandomQuote/RandomQuote.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './RandomQuote.css'
 
 const RandomQuote = () => {
+
+    const [quote,setQuote] = useState({
+        text: "Difficulties increase the nearer we get to the goal." ,
+        author: "Johann wolfgang von Goethe",
+    });
+
   return (
-    <div>
-      
+    <div className='container'>
+      <div className="quote">{quote.text}</div>
+      <div>
+        <div className="line"></div>
+        <div className="bottom">
+            <div className="author">{quote.author}</div>
+            <div className="icons">
+                <img src="" alt="" />
+                <img src="" alt="" />
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
